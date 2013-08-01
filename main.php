2ea9d2b04efbd1ad7665dee5cec499d50d1c38c1
Chakrachai <comma11558@hotmail.com>
2013-08-02 00:29:49 +0700

    design ui

---
+++ main.php
@@ -0,0 +1,66 @@
+<html>
+	<head>
+		<title>Notebookspec</title>
+		<meta charset='utf-8'>
+		<link href="maincss.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+
+		<div id="ext_container">
+		<div class="header_top clearfix">
+		<a id="logo"></a>
+		</div>
+		<div class="header_bottom clearfix"><font color="#ffffff">ตรวจสอบ Space Notebook</font></div><br>
+			<table align=center border=0 width="100%">
+				<tr>
+					<td>
+						<fieldset>
+							<center>กรอกข้อมูลตามที่ต้องการ
+								<form method="post" action="spece.php">
+									
+									Brand : <input type="text" name="band"> 
+									CPU : 
+									<select name="cpu">
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+									</select>
+									<br>
+									Graphic Card : 
+
+									<select name="gp">
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+									</select>
+									ความจุ HDD: 
+
+									<select name="hddsize">
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+									</select>
+									น้ำหนัก :
+									<select name="weith">
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+									</select>
+									
+									<select name="weith">
+										<option value="a">a
+										<option value="a">a
+										<option value="a">a
+									</select>
+								</form>
+							</center>
+						</fieldset>
+					</td>
+				</tr>
+			</table>
+	</body>
+</html>
