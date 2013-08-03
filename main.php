f4f0620079b899438a04b17bd84e61bacd0d23bf
Chakrachai <comma11558@hotmail.com>
2013-08-03 17:20:45 +0700

    ui insert menu

--- main.php
+++ main.php
@@ -16,26 +16,76 @@
 					<td>
 						<fieldset>
 							<center>กรอกข้อมูลตามที่ต้องการ
-								<form method="post" action="spece.php">
-									
-									Brand : <input type="text" name="band"> 
+								<form method="post" action="#">
+									Brand : 
+									<select name="cpu">
+
+									<?php
+											$bandarray=array();
+											$bandcmd = "swipl -q -f notebook.pl -g \"forall(notebooks(BRAND,VERSION,CPUNO,GPCARD,HDD,MEMORY,WEIGTH,SCREEN,OS,PRICE),writeln([BRAND]))\",halt";
+											$output = shell_exec($bandcmd);
+											$output = explode("[", $output);
+											foreach ($output as $key => $value) {
+												$output[$key]=str_replace("]", "", $value);
+											}
+											foreach ($output as $key => $value) {
+												if($output[$key]!=null){
+													//echo "<option value=\"a\">a";
+													if(((array_search($output[$key],$bandarray)))!=true){
+														$bandarray[$key]=$output[$key];
+														echo "<option value=\"".$bandarray[$key]."\">".$bandarray[$key];
+													} 
+												}
+											}
+										?>										
+									</select>
+
 									CPU : 
 									<select name="cpu">
-										<option value="a">a
-										<option value="a">a
-										<option value="a">a
-										<option value="a">a
+
+										<?php
+											$cpucmd = "swipl -q -f notebook.pl -g \"forall(cpus(CPUBRAND,CPUNAME,CPUNUMBER,CPUSPEED,CPUSPEEDUP,CPUCASE),writeln([CPUBRAND,CPUNAME]))\",halt";
+											$output = shell_exec($cpucmd);
+											$output = explode("[", $output);
+											$arrprint =array();
+											foreach ($output as $key => $value) {
+												$output[$key]=str_replace("]", "", $value);
+											}
+											foreach ($output as $key => $value) {
+												$output[$key]=str_replace(",", " ", $value);
+												if($output[$key]!=null){
+													//echo "<option value=\"a\">a";
+													if(((array_search($output[$key], $arrprint)))!=true){
+														echo "<option value=\"".$output[$key]."\">".$output[$key];
+														$arrprint[$key]=$output[$key];
+													} 
+												}
+											}
+										?>
 									</select>
 									<br>
 									Graphic Card : 
 
 									<select name="gp">
-										<option value="a">a
-										<option value="a">a
-										<option value="a">a
-										<option value="a">a
-										<option value="a">a
-										<option value="a">a
+										<?php
+											$gpcmd = "swipl -q -f notebook.pl -g \"forall(gpcard(GPBRAND,GPNO,GPSIZE),writeln([GPBRAND,GPNO]))\",halt.";
+											$output = shell_exec($gpcmd);
+											$output = explode("[", $output);
+											$arrprint =array();
+											foreach ($output as $key => $value) {
+												$output[$key]=str_replace("]", "", $value);
+											}
+											foreach ($output as $key => $value) {
+												$output[$key]=str_replace(",", " ", $value);
+												if($output[$key]!=null){
+													//echo "<option value=\"a\">a";
+													if(((array_search($output[$key], $arrprint)))!=true){
+														echo "<option value=\"".$output[$key]."\">".$output[$key];
+														$arrprint[$key]=$output[$key];
+													} 
+												}
+											}
+										?>
 									</select>
 									ความจุ HDD: 
 
@@ -76,5 +126,6 @@
 					</td>
 				</tr>
 			</table>
+
 	</body>
 </html>
--- notebook.pl
+++ notebook.pl
@@ -1,95 +1,95 @@
 %notebook(BRAND,VERSION,CPUNO,GPCARD,HDD,MEMORY,WEIGTH,SCREEN,OS,PRICE)
 
-notebook(acer,aspireV5,'3317U',geForceGT620M,'500GB','4GB','2.1KG','14N',windows8,25900).
-notebook(acer,asprieS7,'4500U',hdGraphics4400,'256GB','8GB','1.3KG','13.3N',windows8,59900).
-notebook(acer,aspireV3,'3630QM',geForceGT710M,'1TB','4GB','2.4KG','14N',dos,23900).
-notebook(acer,aspireE1,'B2020',geForceGT710M,'500GB','4GB','2.25KG','14N',dos,12900).
-notebook(acer,aspireE1,'2348M',geForceGT710M,'500GB','4GB','2.25KG','14N',dos,14900).
+notebook(acer,aspireV5,'3317U',GeForceGT620M,'500GB','4GB','2.1KG','14N',windows8,25900).
+notebook(acer,asprieS7,'4500U','HD Graphics4400','256GB','8GB','1.3KG','13.3N',windows8,59900).
+notebook(acer,aspireV3,'3630QM','GeForceGT710M','1TB','4GB','2.4KG','14N',dos,23900).
+notebook(acer,aspireE1,'B2020','GeForceGT710M','500GB','4GB','2.25KG','14N',dos,12900).
+notebook(acer,aspireE1,'2348M','GeForceGT710M','500GB','4GB','2.25KG','14N',dos,14900).
 %-------------------------------------
-notebook(apple,'Pro2012','3720M',geForceGT650M,'512GB','8GB','2.02KG','15.4N',macOS,94900).
-notebook(apple,'Air2013','Haswell',hdGraphics5000,'256GB','4GB','1.35KG','13.3N',macOS,38900).
-notebook(apple,'Air2013','Haswell',hdGraphics5000,'256GB','4GB','1.08KG','11.6N',macOS,35900).
-notebook(apple,'Pro2012','3610M',geForceGT650M,'500GB','4GB','2.02KG','15.4N',macOS,59900).
-notebook(apple,'Air2012','3317U',hdGraphics4000,'64GB','4GB','1.08KG','11.6N',macOS,32900).
+notebook(apple,'Pro2012','3720M','GeForceGT650M','512GB','8GB','2.02KG','15.4N',macOS,94900).
+notebook(apple,'Air2013','Haswell','HD Graphics5000','256GB','4GB','1.35KG','13.3N',macOS,38900).
+notebook(apple,'Air2013','Haswell','HD Graphics5000','256GB','4GB','1.08KG','11.6N',macOS,35900).
+notebook(apple,'Pro2012','3610M','GeForceGT650M','500GB','4GB','2.02KG','15.4N',macOS,59900).
+notebook(apple,'Air2012','3317U','HD Graphics4000','64GB','4GB','1.08KG','11.6N',macOS,32900).
 %------------------------------------
-notebook(asus,'K45VJ-VX064D','3230M',geForceGT635M,'500GB','4GB','2.2KG','14N',dos,18900).
-notebook(asus,'K450JF-WX013D','4700HQ',geForceGT745M,'750GB','4GB','2.1KG','14N',dos,24900).
-notebook(asus,'Vivobook S550CB-CJ048H','3537U',geForceGT740M,'1TB','8GB','2.6KG','15.6N',windows8,29900).
-notebook(asus,'A45VM-VX132D','3630QM',geForceGT630M,'750GB','4GB','2.44KG','14N',dos,25900).
-notebook(asus,'G55VX-S1237H','3630QM',geForceGTX660M,'1TB','16GB','3.78KG','15.6N',windows8,49900).
-notebook(asus,'TAICHI 21-CW004H','3517U',hdGraphics4000,'256GB','4GB','1.1KG','11.6N',windows8,45900).
+notebook(asus,'K45VJ-VX064D','3230M','GeForceGT635M','500GB','4GB','2.2KG','14N',dos,18900).
+notebook(asus,'K450JF-WX013D','4700HQ','GeForceGT745M','750GB','4GB','2.1KG','14N',dos,24900).
+notebook(asus,'Vivobook S550CB-CJ048H','3537U','GeForceGT740M','1TB','8GB','2.6KG','15.6N',windows8,29900).
+notebook(asus,'A45VM-VX132D','3630QM','GeForceGT630M','750GB','4GB','2.44KG','14N',dos,25900).
+notebook(asus,'G55VX-S1237H','3630QM','GeForceGTX660M','1TB','16GB','3.78KG','15.6N',windows8,49900).
+notebook(asus,'TAICHI 21-CW004H','3517U','HD Graphics4000','256GB','4GB','1.1KG','11.6N',windows8,45900).
 %------------------------------------
-notebook(benq,'joybook S46B-M07','350M',geForce310M,'320GB','2GB','2.3KG','14N',linux,19900).
+notebook(benq,'joybook S46B-M07','350M','GeForce310M','320GB','2GB','2.3KG','14N',linux,19900).
 notebook(benq,'joybook LiteT131P-M02','210U',radeonX1270,'320GB','2GB','1.5KG','14N',dos,13900).
-notebook(benq,'joybook S46-M04','520M',geForce310M,'320GB','2GB','2.3KG','14N',dos,24900).
+notebook(benq,'joybook S46-M04','520M','GeForce310M','320GB','2GB','2.3KG','14N',dos,24900).
 notebook(benq,'joybook S43C-M08','SU4100',radeonHD4330,'500GB','2GB','1.8KG','14N',linux,19900).
-notebook(benq,'joybook S35-m16','SU2300',hdGraphics4500M,'320GB','2GB','1.4KG','13.3N',linux,14900).
+notebook(benq,'joybook S35-m16','SU2300','HD Graphics4500M','320GB','2GB','1.4KG','13.3N',linux,14900).
 %------------------------------------
 notebook(compaq,'Pavilion G6 1112TX','2410',radeonHD6470M,'640GB','4GB','2.1KG','15.6N',windows7,24900).
-notebook(compaq,'HP 430-441TU','2310M',hdGraphics3000,'500GB','2GB','2KG','14N',dos,15900).
+notebook(compaq,'HP 430-441TU','2310M','HD Graphics3000','500GB','2GB','2KG','14N',dos,15900).
 notebook(compaq,'Presario CQ42-265TU','330M',radeonHD5430,'500GB','2GB','2.44KG','14N',dos,18990).
 notebook(compaq,'Presario CQ42-281TX','450M',radeonTMHD5430,'320GB','2048MB','2.44KG','14N',dos,21990).
 notebook(compaq,'compaq 320','P320',radeonHD530v,'320GB','1024MB','2.04KG','13.3N',linux,15990).
 %------------------------------------
-notebook(fujitsu,'LIFEBOOK SH572','3210M',hdGraphics4000,'750GB','4GB','1.42KG','13.3N',windows7,49900).
-notebook(fujitsu,'LIFEBOOK SH782-i5','3210M',hdGraphics4000,'750GB','4GB','1.31KG','13.3N',windows8,51900).
-notebook(fujitsu,'STYLISTIC Q702','3427U',hdGraphics4000,'120GB','4GB','1.70KG','11.6N',windows8,74800).
-notebook(fujitsu,'LIFEBOOK SH772','3520M',hdGraphics4000,'750GB','4GB','1.21KG','13.3N',windows7,76900).
-notebook(fujitsu,'LIFEBOOK SH782-i7','3520M',hdGraphics4000,'1TB','8GB','1.31KG','13.3N',windows8,79900).
+notebook(fujitsu,'LIFEBOOK SH572','3210M','HD Graphics4000','750GB','4GB','1.42KG','13.3N',windows7,49900).
+notebook(fujitsu,'LIFEBOOK SH782-i5','3210M','HD Graphics4000','750GB','4GB','1.31KG','13.3N',windows8,51900).
+notebook(fujitsu,'STYLISTIC Q702','3427U','HD Graphics4000','120GB','4GB','1.70KG','11.6N',windows8,74800).
+notebook(fujitsu,'LIFEBOOK SH772','3520M','HD Graphics4000','750GB','4GB','1.21KG','13.3N',windows7,76900).
+notebook(fujitsu,'LIFEBOOK SH782-i7','3520M','HD Graphics4000','1TB','8GB','1.31KG','13.3N',windows8,79900).
 %-----------------------------------
 notebook(dell,'Inspiron 3521-W560105TH','3537U',radeonHD8730M,'1TB','4GB','2.24KG','15N',linux,23990).
-notebook(dell,'Vostro 3460-V523402UTHR','3210M',geForceGT630M,'500GB','4GB','2.24KG','14N',ubuntu,19990).
-notebook(dell,'XPS 13-W560107TH-L321X','3537U',hdGraphics4000,'256GB','8GB','1.3KG','13.3N',windows8,45990).
-notebook(dell,'Inspiron N3421-V560109TH','1007U',hdGraphics3000,'500GB','2GB','2.16KG','14N',ubuntu,10990).
-notebook(dell,'Vostro 5460-V560303TH','3230M',geForceGT630M,'500GB','4GB','1.54KG','14N',ubuntu,19900).
+notebook(dell,'Vostro 3460-V523402UTHR','3210M','GeForceGT630M','500GB','4GB','2.24KG','14N',ubuntu,19990).
+notebook(dell,'XPS 13-W560107TH-L321X','3537U','HD Graphics4000','256GB','8GB','1.3KG','13.3N',windows8,45990).
+notebook(dell,'Inspiron N3421-V560109TH','1007U','HD Graphics3000','500GB','2GB','2.16KG','14N',ubuntu,10990).
+notebook(dell,'Vostro 5460-V560303TH','3230M','GeForceGT630M','500GB','4GB','1.54KG','14N',ubuntu,19900).
 %-----------------------------------
-notebook(gateway,'NV49C186t','460M',geForceGT330M,'640GB','2GB','2.2KG','14N',windows7,28900).
-notebook(gateway,'EC1812t','SU7300',accelerator4500MHD,'500GB','2GB','1.4KG','11.6N',windows7,28900).
+notebook(gateway,'NV49C186t','460M','GeForceGT330M','640GB','2GB','2.2KG','14N',windows7,28900).
+notebook(gateway,'EC1812t','SU7300','Accelerator4500MHD','500GB','2GB','1.4KG','11.6N',windows7,28900).
 notebook(gateway,'LT3105t/Y030','L110',radeonX1270,'160GB','1GB','1.358KG','11.6N',windows7,17900).
 notebook(gateway,'NV59C13t','520M',radeonHD5470,'640GB','2GB','2.6KG','15.6N',windows7,35900).
-notebook(gateway,'TC7901t/X002','P7550',geForceG105M,'320GB','4GB','2.4KG','14N',windows7,55900).
+notebook(gateway,'TC7901t/X002','P7550','GeForceG105M','320GB','4GB','2.4KG','14N',windows7,55900).
 %------------------------------------
 notebook(hp,'Pavilion G4-1309au','3305M',radeonHD6480,'500GB','4GB','2.1KG','14N',dos,10900).
-notebook(hp,'1000-1332TU','3110M',hdGraphics4000,'500GB','4GB','2.4KG','14N',dos,14990).
-notebook(hp,'Pavilion TouchSmart Sleekbook 15-b115TX','3337U',geForceGT630M,'750GB','8GB','2.4KG','15.6N',windows8,27990).
+notebook(hp,'1000-1332TU','3110M','HD Graphics4000','500GB','4GB','2.4KG','14N',dos,14990).
+notebook(hp,'Pavilion TouchSmart Sleekbook 15-b115TX','3337U','GeForceGT630M','750GB','8GB','2.4KG','15.6N',windows8,27990).
 notebook(hp,'450-791TX','3210M',radeonHD7450,'750GB','4GB','2KG','14N',dos,17990).
-notebook(hp,'Envy DV6-7305TX','3630QM',geForceGT635M,'1TB','8GB','2.44KG','15.6N',windows8,39990).
+notebook(hp,'Envy DV6-7305TX','3630QM','GeForceGT635M','1TB','8GB','2.44KG','15.6N',windows8,39990).
 %------------------------------------
-notebook(lenovo,'Thinkpad Edge-E330-3354A23/3354A22','3210M',hdGraphics4000,'500GB','4GB','1.63KG','13.3N',dos,22900).
-notebook(lenovo,'IdeaPad Y570 59320796','2670QM',geForceGT555M,'750GB','8GB','2.7KG','15.6N',windows7,32990).
+notebook(lenovo,'Thinkpad Edge-E330-3354A23/3354A22','3210M','HD Graphics4000','500GB','4GB','1.63KG','13.3N',dos,22900).
+notebook(lenovo,'IdeaPad Y570 59320796','2670QM','GeForceGT555M','750GB','8GB','2.7KG','15.6N',windows7,32990).
 notebook(lenovo,'G-Series G470','2450M',radeonHD6730M,'500GB','4GB','2.25KG','14.1N',dos,17990).
 notebook(lenovo,'G-Series G475','E-450',radeonHD6320,'750GB','2GB','2KG','14.1N',dos,12990).
-notebook(lenovo,'IdeaPad U300s','2677M',hdGraphics3000,'256GB','4GB','1.34KG','13N',windows7,50990).
+notebook(lenovo,'IdeaPad U300s','2677M','HD Graphics3000','256GB','4GB','1.34KG','13N',windows7,50990).
 %------------------------------------
-notebook(msi,'GE60K 0ND-619XTH','3630QM',geForceGTX660M,'1TB','8GB','2.4KG','15.6N',dos,34900).
-notebook(msi,'GE70 0ND-466XTH','3630QM',geForceGT660X,'1TB','8GB','2.79KG','17.3N',windows8,52900).
-notebook(msi,'GT70 0NE-107TH','3630QM',geForceGTX680M,'750GB','16GB','3.9KG','17.3N',windows8,89900).
-notebook(msi,'GT70R','3610M',geForceGTX670M,'750GB','16GB','3.9KG','17.3N',windows7,89900).
-notebook(msi,'GT683DX','2450M',geForceGTX570M,'750GB','8GB','3.5KG','15.6N',dos,46900).
+notebook(msi,'GE60K 0ND-619XTH','3630QM','GeForceGTX660M','1TB','8GB','2.4KG','15.6N',dos,34900).
+notebook(msi,'GE70 0ND-466XTH','3630QM','GeForceGT660X','1TB','8GB','2.79KG','17.3N',windows8,52900).
+notebook(msi,'GT70 0NE-107TH','3630QM','GeForceGTX680M','750GB','16GB','3.9KG','17.3N',windows8,89900).
+notebook(msi,'GT70R','3610M','GeForceGTX670M','750GB','16GB','3.9KG','17.3N',windows7,89900).
+notebook(msi,'GT683DX','2450M','GeForceGTX570M','750GB','8GB','3.5KG','15.6N',dos,46900).
 %------------------------------------
-notebook(samsung,'ATIV Book 9 NP900X3E-K01TH','3537U',hdGraphics4000,'256GB','4GB','1.13KG','13.3N',windows8,45900).
-notebook(samsung,'ATIV Book 4 NP470R4E-K01TH','3230M',hdGraphics4000,'500GB','4GB','1.81KG','14N',windows8,22900).
-notebook(samsung,'ATIV Book 4 NP450R4V-X02TH','3120M',geForceGT710M,'500GB','4GB','1.81KG','14N',dos,18900).
+notebook(samsung,'ATIV Book 9 NP900X3E-K01TH','3537U','HD Graphics4000','256GB','4GB','1.13KG','13.3N',windows8,45900).
+notebook(samsung,'ATIV Book 4 NP470R4E-K01TH','3230M','HD Graphics4000','500GB','4GB','1.81KG','14N',windows8,22900).
+notebook(samsung,'ATIV Book 4 NP450R4V-X02TH','3120M','GeForceGT710M','500GB','4GB','1.81KG','14N',dos,18900).
 notebook(samsung,'ATIV Book 2 NP275E4V-X01TH','2000',radeonHD8750M,'500GB','2GB','2KG','14N',dos,13900).
-notebook(samsung,'NP370R4E-A02TH','3110M',hdGraphics4000,'500GB','4GB','1.81KG','14N',windows8,19900).
+notebook(samsung,'NP370R4E-A02TH','3110M','HD Graphics4000','500GB','4GB','1.81KG','14N',windows8,19900).
 %-------------------------------------
-notebook(sony,'VAIO S SVS13133','3230M',geForceGT640M,'750GB','4GB','1.72KG','13.3N',windows8,35900).
-notebook(sony,'VAIO T SVT13133','3337U',hdGraphics4000,'500GB','4GB','1.6KG','13.3N',windows8,29900).
-notebook(sony,'VAIO T SVT14117CHS','3517U',hdGraphics4000,'500GB','4GB','1.92KG','14N',windows8,34900).
-notebook(sony,'VAIO S SVS15125CHB','3210M',geForceGT640M,'750GB','4GB','2KG','15.6N',windows8,36900).
-notebook(sony,'VAIO S SVS13A15','3520M',geForceGT640M,'750GB','8GB','1.75KG','13.3N',windows8,47900).
+notebook(sony,'VAIO S SVS13133','3230M','GeForceGT640M','750GB','4GB','1.72KG','13.3N',windows8,35900).
+notebook(sony,'VAIO T SVT13133','3337U','HD Graphics4000','500GB','4GB','1.6KG','13.3N',windows8,29900).
+notebook(sony,'VAIO T SVT14117CHS','3517U','HD Graphics4000','500GB','4GB','1.92KG','14N',windows8,34900).
+notebook(sony,'VAIO S SVS15125CHB','3210M','GeForceGT640M','750GB','4GB','2KG','15.6N',windows8,36900).
+notebook(sony,'VAIO S SVS13A15','3520M','GeForceGT640M','750GB','8GB','1.75KG','13.3N',windows8,47900).
 %-------------------------------------
 notebook(svoa,'cupid srv c2d20000','T5800',m672,'250GB','2GB','1.98KG','12.1N',windows7,25900).
-notebook(svoa,'Apollo B4100M','460M',geForce310M,'500GB','2GB','2.2KG','14N',linux,19000).
-notebook(svoa,'Electra E4125','370M',gma,'320GB','2GB','2.2KG','14N',linux,14500).
-notebook(svoa,'Hasee CG430','370M',gma,'320GB','2GB','2.2KG','14N',linux,15800).
-notebook(svoa,'Hermes SU2700','SU2700',accelerator4500M,'320GB','2GB','1.7KG','13.3N',dos,22990).
+notebook(svoa,'Apollo B4100M','460M','GeForce310M','500GB','2GB','2.2KG','14N',linux,19000).
+notebook(svoa,'Electra E4125','370M','GMA','320GB','2GB','2.2KG','14N',linux,14500).
+notebook(svoa,'Hasee CG430','370M','GMA','320GB','2GB','2.2KG','14N',linux,15800).
+notebook(svoa,'Hermes SU2700','SU2700','Accelerator4500M','320GB','2GB','1.7KG','13.3N',dos,22990).
 %--------------------------------------
-notebook(toshiba,'Satellite U940-1006X','3337U',geForceGT630M,'640GB','4GB','1.7KG','14N',windows8,30990).
-notebook(toshiba,'Satellite U840W-1002','3317U',hdGraphics4000,'500GB','6GB','1.57KG','14N',windows7,44990).
+notebook(toshiba,'Satellite U940-1006X','3337U','GeForceGT630M','640GB','4GB','1.7KG','14N',windows8,30990).
+notebook(toshiba,'Satellite U840W-1002','3317U','HD Graphics4000','500GB','6GB','1.57KG','14N',windows7,44990).
 notebook(toshiba,'Satellite L840-1021X','3110M',radeonHD7670,'640GB','4GB','2.2KG','14N',dos,30900).
-notebook(toshiba,'Portege Z930-20060T','3667U',hdGraphics4000,'128GB','4GB','1.12KG','13.3N',windows7,52990).
-notebook(toshiba,'Qosmio F770-1006XT','2670M',geForceGTX560M,'1.5TB','8GB','2.8KG','15.6N',windows7,71900).
+notebook(toshiba,'Portege Z930-20060T','3667U','HD Graphics4000','128GB','4GB','1.12KG','13.3N',windows7,52990).
+notebook(toshiba,'Qosmio F770-1006XT','2670M','GeForceGTX560M','1.5TB','8GB','2.8KG','15.6N',windows7,71900).
 
 %========================================rule===================================
 %cpu(CPUBRAND,CPUNAME,CPUNUMBER,CPUSPEED,CPUSPEEDUP,CPUCASE).
@@ -144,35 +144,35 @@ cpu(amd,'E-Series','E-450','1.65GHz','-','1MB L2').
 
 %card(GPBRAND,GPNO,GPSIZE).
 
-card(nVidia,geForce310M,'512MB').
-card(nVidia,geForceG105M,'1.2GB').
-card(nVidia,geForceGT310M,'512MB').
-card(nVidia,geForceGT330M,'1GB').
-card(nVidia,geForceGT555M,'2GB').
-card(nVidia,geForceGT630M,'4GB').
-card(nVidia,geForceGT635M,'2GB').
-card(nVidia,geForceGT640M,'2GB').
-card(nVidia,geForceGT650M,'1GB').
-card(nVidia,geForceGT710M,'2GB').
-card(nVidia,geForceGT740M,'2GB').
-card(nVidia,geForceGT745M,'2GB').
+card(nVidia,'GeForce310M','512MB').
+card(nVidia,'GeForceG105M','1.2GB').
+card(nVidia,'GeForceGT310M','512MB').
+card(nVidia,'GeForceGT330M','1GB').
+card(nVidia,'GeForceGT555M','2GB').
+card(nVidia,'GeForceGT630M','4GB').
+card(nVidia,'GeForceGT635M','2GB').
+card(nVidia,'GeForceGT640M','2GB').
+card(nVidia,'GeForceGT650M','1GB').
+card(nVidia,'GeForceGT710M','2GB').
+card(nVidia,'GeForceGT740M','2GB').
+card(nVidia,'GeForceGT745M','2GB').
 
-card(nVidia,geForceGTX560M,'2GB').
-card(nVidia,geForceGTX570M,'1.5GB').
-card(nVidia,geForceGTX660M,'2GB').
-card(nVidia,geForceGTX670M,'3GB').
-card(nVidia,geForceGTX680M,'2GB').
-card(nVidia,geForceGT660X,'2GB').
+card(nVidia,'GeForceGTX560M','2GB').
+card(nVidia,'GeForceGTX570M','1.5GB').
+card(nVidia,'GeForceGTX660M','2GB').
+card(nVidia,'GeForceGTX670M','3GB').
+card(nVidia,'GeForceGTX680M','2GB').
+card(nVidia,'GeForceGT660X','2GB').
 
-card(intel,accelerator4500MHD,'-').
-card(intel,accelerator4500M,'256MB').
-card(intel,gma,'-').
+card(intel,'Accelerator4500MHD','-').
+card(intel,'Accelerator4500M','256MB').
+card(intel,'GMA','-').
 
-card(intel,hdGraphics3000,'-').
-card(intel,hdGraphics4000,'-').
-card(intel,hdGraphics4400,'-').
-card(intel,hdGraphics4500M,'-').
-card(intel,hdGraphics5000,'-').
+card(intel,'HD Graphics3000','-').
+card(intel,'HD Graphics4000','-').
+card(intel,'HD Graphics4400','-').
+card(intel,'HD Graphics4500M','-').
+card(intel,'HD Graphics5000','-').
 
 card(ati,radeonX1270,'-').
 card(ati,radeonHD4330,'512MB').
@@ -190,3 +190,13 @@ card(ati,radeonHD8750M,'1GB').
 card(ati,radeonHD7670,'2GB').
 
 card(sis,m672,'-').
+
+notebooks(BRAND,VERSION,CPUNO,GPCARD,HDD,MEMORY,WEIGTH,SCREEN,OS,PRICE) :- 
+	  notebook(BRAND,VERSION,CPUNO,GPCARD,HDD,MEMORY,WEIGTH,SCREEN,OS,PRICE).
+cpus(CPUBRAND,CPUNAME,CPUNUMBER,CPUSPEED,CPUSPEEDUP,CPUCASE) :-
+		cpu(CPUBRAND,CPUNAME,CPUNUMBER,CPUSPEED,CPUSPEEDUP,CPUCASE).
+gpcard(GPBRAND,GPNO,GPSIZE) :-
+  		card(GPBRAND,GPNO,GPSIZE).
+
+notebookspeck(BRAND,VERSION,CPUNO,GPCARD,HDD,MEMORY,WEIGTH,SCREEN,OS,PRICE,CPUBRAND,CPUNAME,CPUSPEED,CPUSPEEDUP,CPUCASE,GPBRAND,GPSIZE) :-
+  		notebooks(BRAND,VERSION,CPUNO,GPCARD,HDD,MEMORY,WEIGTH,SCREEN,OS,PRICE),cpus(CPUBRAND,CPUNAME,CPUNO,CPUSPEED,CPUSPEEDUP,CPUCASE),gpcard(GPBRAND,GPCARD,GPSIZE).
